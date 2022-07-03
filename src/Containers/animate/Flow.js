c000df56f4a7beb18393596a034341e45f33ea5a
slimane <progj4c@gmail.com>
2022-07-03 20:01:20 +0200

    fisrt8

--- src/Containers/animate/Flow.js
+++ src/Containers/animate/Flow.js
@@ -15,17 +15,17 @@ const SliderData = [
   {
     image:
      require('../../Assets/domaine.jpg'),
-    domaine:"domaine d'activité"
+    domaine:"Domaine d'activité"
   },
   {
     image:
     require('../../Assets/back3.jpg'),
-    domaine:"blog"
+    domaine:"Blog"
   },
   {
     image:
       'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
-      domaine:"moyens",
+      domaine:"Moyens",
   
     },
  
